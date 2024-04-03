2dab974d4f4dde620a655b00ac629c12e335a7d2
Alison <alisonmozer@gmail.com>
2024-04-02 22:18:20 -0300

    testando dapper

---
+++ API/Data/DbSession.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using Npgsql;
+
+public class DbSession : IDisposable
+{
+    public IDbConnection Connection { get; }
+
+    public DbSession(string connectionString)
+    {
+        Connection = new NpgsqlConnection(connectionString);
+        Connection.Open();
+    }
+
+    public void Dispose()
+    {
+        Connection?.Dispose();
+    }
+}
